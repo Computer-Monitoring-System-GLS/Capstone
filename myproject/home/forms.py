69338d74c48a7f7799a46ab83885e5c647edfb2f
priyal1011 <139989173+priyal1011@users.noreply.github.com>
2025-07-26 22:14:53 +0530

    Delete myproject directory

--- myproject/home/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    serial_no = forms.CharField(max_length=100, label="Serial Number")
-    password = forms.CharField(widget=forms.PasswordInput, label="Password")
